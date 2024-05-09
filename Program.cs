2ef4749dcd31b4b94fb95763bcd17607d4b1e967
isaque silva <isaque.silva@equilibriumweb.com>
2024-05-09 15:28:18 -0300

    adicionando poco e brisa

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
         Console.WriteLine("Opção 2. 5 por 4");
         Console.WriteLine("Opção 3. 6 por 6");
 
-        ImprimirMatrizComGrades();
+      
 
         int opcao;
         do
@@ -39,33 +39,122 @@ class Program
                 colunas = 4;
                 break;
         }
-
         // Criando a matriz escolhida
         int[,] matriz = new int[linhas, colunas];
 
-        // Exibindo a matriz para verificar se está correta
+        // Definindo as percepções (pocos e brisas)
+        DefinirPercepcoes(matriz, 3, 2);
+
+        // Exibindo a matriz com as percepções
+        ImprimirMatrizComPercepcoes(matriz);
+
+        /* / Exibindo a matriz para verificar se está correta
         Console.WriteLine($"Matriz criada: {linhas} linhas x {colunas} colunas");
 
         for (int i = 0; i < linhas; i++)
         {
-
+            
             for (int j = 0; j < colunas; j++)
             {
+                
                 Console.Write("|");
 
-                Console.Write($"____");
+                Console.Write($"___");
             }
             Console.WriteLine("|");
         }
+        */
+
 
 
 
     }
 
-    static void ImprimirMatrizComGrades()
+    static void DefinirPercepcoes(int[,] matriz, int numeroPocos, int numeroBrisas)
     {
-        Console.WriteLine("chamou! imprimir");
+        Random rand = new Random();
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        // Define poços (representados pelo valor 1)
+        for (int i = 0; i < numeroPocos; i++)
+        {
+            int linha = rand.Next(0, linhas);
+            int coluna = rand.Next(0, colunas);
+            matriz[linha, coluna] = 1;
+        }
+
+        // Define brisas (representadas pelo valor 2)
+        for (int i = 0; i < numeroBrisas; i++)
+        {
+            int linha = rand.Next(0, linhas);
+            int coluna = rand.Next(0, colunas);
+            if (matriz[linha, coluna] != 1) // Evita colocar brisa onde há poço
+            {
+                matriz[linha, coluna] = 2;
+            }
+        }
     }
+    static void ImprimirMatrizComPercepcoes(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        Console.WriteLine($"Matriz criada: {linhas} linhas x {colunas} colunas");
+
+        // Imprime a linha de grades superior
+        Console.Write("┌");
+        for (int i = 0; i < colunas; i++)
+        {
+            Console.Write("--------");
+        }
+        Console.WriteLine("┐");
+
+        // Imprime o conteúdo da matriz com percepções
+        for (int i = 0; i < linhas; i++)
+        {
+            Console.Write("| ");
+            for (int j = 0; j < colunas; j++)
+            {
+                if (matriz[i, j] == 1) // Percepção de poço
+                {
+                    Console.Write(" Poco ");
+                }
+                else if (matriz[i, j] == 2) // Percepção de brisa
+                {
+                    Console.Write(" Brisa");
+                }
+                else // Célula normal
+                {
+                    Console.Write("     ");
+                }
+                Console.Write(" | ");
+            }
+            Console.WriteLine();
+
+            // Imprime a linha de grades entre as linhas da matriz
+            if (i < linhas - 1)
+            {
+                Console.Write("├");
+                for (int k = 0; k < colunas; k++)
+                {
+                    Console.Write("--------");
+                }
+                Console.WriteLine("┤");
+            }
+        }
+
+        // Imprime a linha de grades inferior
+        Console.Write("└");
+        for (int i = 0; i < colunas; i++)
+        {
+            Console.Write("--------");
+        }
+        Console.WriteLine("┘");
+    }
+
+
+
 }
 
 
